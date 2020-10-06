172110cfa9c72ee7a67bf5c63066bdfeeb510d21
Souptik <souptikdatta2001@gmail.com>
2020-10-07 03:19:41 +0530

    Added mysql connection and one rout

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,19 @@ app.use(express.json());
 // This is for avoiding the CORS error that is we can call the backend API from the browser
 app.use(cors());
 
+var connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST || 'localhost',
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASS || '',
+    database: process.env.MYSQL_DB || 'nodedb'
+});
+connection.connect((err) => {
+    if (err) { console.log(err); } else { console.log("Connected to Database Succesfully..."); }
+});
+
+app.get('/perMonth', ()=>{
+    connection.query('SELECT expense, month FROM expenses WHERE user=? ORDER BY month DESC LIMIT 10', [req.query.user])
+});
 
 
 app.listen(process.env.PORT || 3000, ()=>{
