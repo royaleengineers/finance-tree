166f3383abafb69245edffb63890da747347c64a
Mayukh Bhattacharya <mayukh1978@gmail.com>
2020-10-07 03:13:40 +0530

    added arrow button

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import {Header} from './Components/Header';
 import LineChart from './Components/LineChart';
+import Carousel from 'react-elastic-carousel';
+import ClipLoader from "react-spinners/ClipLoader";
+import { css } from "@emotion/core";
 import './App.css';
+const loaderCSS=css`
+margin-top: 25px;
+margin-bottom: 25px;
+`
 
 function App() {
   return (
     <div>
       <Header/>
+      <ClipLoader css={loaderCSS} size={24} color= 'black' loading/>
+      <Carousel>
+      <LineChart />
+
+      </Carousel>
       <div className='chart'>
-        <LineChart />
+        
       </div>
       </div>
 
